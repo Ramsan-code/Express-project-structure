5a09625ee9bd986fe25cdf17ef192c68d0f7f8c3
thaventhiran Ramsan <ramsandota@gmail.com>
2025-09-29 14:33:43 +0530

    change

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 // Basic server setup
-import express from "express";
+import express, { Router } from "express";
 
 const app = express();
+const router = express.Router();
 const PORT = 3000;
 
 // HTTP method handling
-app.get("/", (req, res) => {
-  res.send("Hello Express!");
-});
 
-app
-  .route("/book")
-  .get((req, res) => {
-    res.send("Get a random book");
-  })
-  .post((req, res) => {
-    res.send("Add a book");
-  })
-  .put((req, res) => {
-    res.send("Update the book");
-  });
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
+router.get("/user", (req, res) => {
+  res.send("this is get user");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
